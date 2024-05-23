2bcbc02b267506c6bc8298d41ec48c38b43ad18a
JOSE_DEV_ALVCOM <jlsc.hco96@gmail.com>
2024-05-22 19:08:24 -0500

    L 220524_5

--- bdapt.sql
+++ bdapt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-05-2024 a las 01:40:06
+-- Tiempo de generación: 23-05-2024 a las 02:07:37
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.1.17
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bd_apuesta_total`
+-- Base de datos: `bdapt`
 --
 
 DELIMITER $$
@@ -27,17 +27,19 @@ DELIMITER $$
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `clientPayData` (IN `client_id` INT)   BEGIN
      IF client_id = 0 THEN
-        SELECT cp.id, c.id as id_client, cp.amount, c.player_id, c.name, c.last_pat, c.last_mat, cp.day, cp.hour, ca.channel, e.names, cp.bank 
-        FROM client_pay cp
-        LEFT JOIN client c ON c.id = cp.client_id
-        LEFT JOIN channel_attention ca ON ca.id = cp.channel_attention_id
-        LEFT JOIN employed e ON e.id = cp.employed_id;
-    ELSE
-        SELECT cp.id, c.id as id_client, cp.amount, c.player_id, c.name, c.last_pat, c.last_mat, cp.day, cp.hour, ca.channel, e.names, cp.bank 
+        SELECT cp.id, c.id as id_client, cp.amount, c.player_id, c.name, c.last_pat, c.last_mat, cp.day, cp.hour, ca.channel, e.names, b.bank 
         FROM client_pay cp
         LEFT JOIN client c ON c.id = cp.client_id
         LEFT JOIN channel_attention ca ON ca.id = cp.channel_attention_id
         LEFT JOIN employed e ON e.id = cp.employed_id
+        LEFT JOIN bank b ON b.id = cp.bank_id;
+    ELSE
+        SELECT cp.id, c.id as id_client, cp.amount, c.player_id, c.name, c.last_pat, c.last_mat, cp.day, cp.hour, ca.channel, e.names, b.bank 
+        FROM client_pay cp
+        LEFT JOIN client c ON c.id = cp.client_id
+        LEFT JOIN channel_attention ca ON ca.id = cp.channel_attention_id
+        LEFT JOIN employed e ON e.id = cp.employed_id
+        LEFT JOIN bank b ON b.id = cp.bank_id
         WHERE c.id = client_id;
     END IF;
     END$$
@@ -52,9 +54,17 @@ DELIMITER ;
 
 CREATE TABLE `bank` (
   `id` int(11) NOT NULL,
-  `bank` varchar(100) DEFAULT NULL,
-  `flag` tinyint(1) DEFAULT NULL
+  `bank` varchar(45) NOT NULL,
+  `flag` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `bank`
+--
+
+INSERT INTO `bank` (`id`, `bank`, `flag`) VALUES
+(1, 'INTERBANK', ''),
+(2, 'BCP', '');
 
 -- --------------------------------------------------------
 
@@ -116,24 +126,14 @@ CREATE TABLE `client_pay` (
   `id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
   `amount` decimal(11,2) NOT NULL,
-  `bank_id` int(11) NOT NULL,
   `day` date NOT NULL,
   `hour` time NOT NULL,
   `url_img` varchar(100) NOT NULL,
   `flag` tinyint(4) NOT NULL,
   `employed_id` int(11) NOT NULL,
-  `channel_attention_id` int(11) NOT NULL
+  `channel_attention_id` int(11) NOT NULL,
+  `bank_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `client_pay`
---
-
-INSERT INTO `client_pay` (`id`, `client_id`, `amount`, `bank_id`, `day`, `hour`, `url_img`, `flag`, `employed_id`, `channel_attention_id`) VALUES
-(1, 2, 100.00, 0, '2023-05-10', '10:30:00', 'EVEDENCIA_2_2024-05-22 00:08:20.jpg', 1, 1, 1),
-(2, 1, 250.00, 0, '2024-12-15', '14:00:00', 'EVEDENCIA_1_2024-05-22 00:11:07.jpg', 1, 1, 3),
-(3, 1, 1500.00, 0, '2024-04-10', '14:23:00', 'EVEDENCIA_1_1716354960.jpg', 1, 1, 2),
-(4, 2, 1500.00, 0, '2024-01-01', '15:40:00', 'EVEDENCIA_2_1716356488.jpg', 1, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -151,6 +151,7 @@ CREATE TABLE `client_pay_log` (
   `employed_id` int(11) DEFAULT NULL,
   `channel_attention_id` int(11) DEFAULT NULL,
   `flag` tinyint(4) DEFAULT NULL,
+  `bank_id` int(11) DEFAULT NULL,
   `type_log` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -178,6 +179,32 @@ CREATE TABLE `employed` (
 INSERT INTO `employed` (`id`, `names`, `last_pat`, `last_mat`, `type_doc`, `num_doc`, `status`, `profile_id`) VALUES
 (1, 'ANGEL', 'VALERIO', 'GARCIA', 1, 45895612, 1, 1),
 (2, 'ALEXANDRA', 'RAMIREZ', 'CAJALEON', 1, 78561245, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modification_type`
+--
+
+CREATE TABLE `modification_type` (
+  `id` int(11) NOT NULL,
+  `modification_type` varchar(45) NOT NULL,
+  `flag` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pay_modify`
+--
+
+CREATE TABLE `pay_modify` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `client_pay_id` int(11) NOT NULL,
+  `modification_type_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -228,7 +255,8 @@ ALTER TABLE `client_pay`
   ADD PRIMARY KEY (`id`),
   ADD KEY `client_id` (`client_id`),
   ADD KEY `employed_id` (`employed_id`),
-  ADD KEY `channel_attention_id` (`channel_attention_id`);
+  ADD KEY `channel_attention_id` (`channel_attention_id`),
+  ADD KEY `bank_id` (`bank_id`);
 
 --
 -- Indices de la tabla `client_pay_log`
@@ -244,6 +272,20 @@ ALTER TABLE `employed`
   ADD KEY `profile_id` (`profile_id`);
 
 --
+-- Indices de la tabla `modification_type`
+--
+ALTER TABLE `modification_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pay_modify`
+--
+ALTER TABLE `pay_modify`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_pay_id` (`client_pay_id`),
+  ADD KEY `modification_type_id` (`modification_type_id`);
+
+--
 -- Indices de la tabla `profile`
 --
 ALTER TABLE `profile`
@@ -257,7 +299,7 @@ ALTER TABLE `profile`
 -- AUTO_INCREMENT de la tabla `bank`
 --
 ALTER TABLE `bank`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `channel_attention`
@@ -290,6 +332,18 @@ ALTER TABLE `employed`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `modification_type`
+--
+ALTER TABLE `modification_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pay_modify`
+--
+ALTER TABLE `pay_modify`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `profile`
 --
 ALTER TABLE `profile`
@@ -305,13 +359,21 @@ ALTER TABLE `profile`
 ALTER TABLE `client_pay`
   ADD CONSTRAINT `client_pay_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `client_pay_ibfk_2` FOREIGN KEY (`employed_id`) REFERENCES `employed` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `client_pay_ibfk_3` FOREIGN KEY (`channel_attention_id`) REFERENCES `channel_attention` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `client_pay_ibfk_3` FOREIGN KEY (`channel_attention_id`) REFERENCES `channel_attention` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `client_pay_ibfk_4` FOREIGN KEY (`bank_id`) REFERENCES `bank` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `employed`
 --
 ALTER TABLE `employed`
   ADD CONSTRAINT `employed_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `pay_modify`
+--
+ALTER TABLE `pay_modify`
+  ADD CONSTRAINT `pay_modify_ibfk_1` FOREIGN KEY (`client_pay_id`) REFERENCES `client_pay` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pay_modify_ibfk_2` FOREIGN KEY (`modification_type_id`) REFERENCES `modification_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
